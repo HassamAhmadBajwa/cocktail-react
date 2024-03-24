f75ade33b51b51cd6173c04afe1dde87c0becad6
Hassam Ahmad Bajwa <hassamahmadbajwa@gmail.com>
2024-03-25 03:04:37 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cocktail-project-react/",
   plugins: [react()],
-  base: "/react-cocktail-project",
 });
